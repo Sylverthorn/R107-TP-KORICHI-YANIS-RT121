49f65872170a061e127df21c5e61d121ff5dc945
Sylverthorn <97361294+Sylverthorn@users.noreply.github.com>
2023-12-19 17:18:45 +0100

    Add files via upload

---
+++ TP5/EXO3_Palindromes.py
@@ -0,0 +1,9 @@
+Phrase = input("Entrez un mot ou une phrase : ")
+
+S1 = S1.split(Phrase)
+Long = len(Phrase)
+
+for i in range (Long):
+    if S1[i] == , or   or /:
+        S1.remove(S1[i])
+print (S1)
