77a34fb26f6ace78cbaf0a66df1c5db2bc2cd89d
Sylverthorn <97361294+Sylverthorn@users.noreply.github.com>
2023-12-23 16:06:46 +0100

    Delete TP5 directory

--- TP5/EXO3_Palindromes.py
+++
@@ -1,9 +0,0 @@
-Phrase = input("Entrez un mot ou une phrase : ")
-
-S1 = S1.split(Phrase)
-Long = len(Phrase)
-
-for i in range (Long):
-    if S1[i] == , or   or /:
-        S1.remove(S1[i])
-print (S1)
